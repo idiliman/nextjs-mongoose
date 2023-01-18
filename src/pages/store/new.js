5648f39bcfe6f3b1858eaa3f80ce1085272322c9
idil <idil.iman1999@gmail.com>
2023-01-18 18:27:08 +0800

    feat: Implement useReducer to catch store event

--- src/pages/store/new.js
+++ src/pages/store/new.js
@@ -1,45 +1,69 @@
-import { useReducer, useState } from "react";
+import { useState, useReducer } from "react";
 
-// const storeReducer = (state, event) => {
-//   return {
-//     ...state,
-//     [event.target.name]: event.target.value,
-//   };
-// };
+const initialValue = {
+  storeName: "",
+  storeAddress: "",
+};
+
+const storeReducer = (state, action) => {
+  switch (action.type) {
+    case "update":
+      return {
+        ...state,
+        [action.payload.key]: action.payload.value,
+      };
+    case "submit": {
+      return initialValue;
+    }
+    default:
+      throw new Error(`Unknown action type: ${action.type}`);
+  }
+};
 
 function store() {
-  // const [formData, setFormData] = useReducer(storeReducer, {
-  //   storeName: "",
-  //   storeAddress: "",
-  // });
-  const [formData, setFormData] = useState({
-    storeName: "",
-    storeAddress: "",
-  });
-  console.log("formData", formData);
+  const [storeState, dispatchStore] = useReducer(storeReducer, initialValue);
+  // console.log("formData", storeState);
+
+  const inputAction = (event) => {
+    dispatchStore({
+      type: "update",
+      payload: { key: event.target.name, value: event.target.value },
+    });
+  };
 
   // Submitting data to API
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    if (Object.keys(formData).length == 0) {
+    if (storeState.storeName === initialValue.storeName) {
       return console.log("Don't have Form Data");
     }
 
-    let { storeName, storeAddress } = formData;
+    let { storeName, storeAddress } = storeState;
 
     const model = {
       name: `${storeName}`,
       address: `${storeAddress}`,
     };
 
-    setFormData({
-      storeName: "",
-      storeAddress: "",
-    });
-
     console.log(model);
+    console.log("Submitted");
+
+    dispatchStore({
+      type: "submit",
+    });
   };
+
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+
+  //   setFormData((state) => {
+  //     return {
+  //       ...state,
+  //       [name]: value,
+  //     };
+  //   });
+  // };
 
   return (
     <div>
@@ -48,8 +72,8 @@ function store() {
       <form onSubmit={handleSubmit}>
         <div>
           <input
-            onChange={setFormData}
-            value={formData.name}
+            onChange={inputAction}
+            value={storeState.storeName}
             type="text"
             name="storeName"
             placeholder="Store Name"
@@ -57,7 +81,8 @@ function store() {
         </div>
         <div>
           <input
-            onChange={setFormData}
+            onChange={inputAction}
+            value={storeState.storeAddress}
             type="text"
             name="storeAddress"
             placeholder="Store Address"
